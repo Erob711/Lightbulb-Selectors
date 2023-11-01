1037f1daf00d3692b94cc4113c96fad08456c6c0
Erob711 <emattroberson@gmail.com>
2023-11-01 15:35:23 -0700

    added event listeners for lightbulbs

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Write your code here
+console.log("bruuuh");
+
+let leftLight = document.getElementById("lightbulb1");
+let midLight = document.getElementById("lightbulb2");
+let rightLight = document.getElementById("lightbulb3");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+addToEventListener(leftLight);
+addToEventListener(midLight);
+addToEventListener(rightLight);
+
+function addToEventListener(element) {
+  element.addEventListener("click", function () {
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+    element.classList.toggle("active");
+  });
+}
